fe101732e50d632a7fbed395be9ef032dfc88148
steeltx <realsteel.gnu@gmail.com>
2021-08-03 23:55:50 -0500

    Ejercicio ternarios

---
+++ src/bases/14-ternarios.js
@@ -0,0 +1,11 @@
+let firstName;
+
+let lastName = 'Herrera';
+
+console.log(`${firstName || 'No firstName'} ${lastName || 'No lastName'}`)
+
+const isActive = true
+
+const message = (isActive) ? 'Activo' : 'Inactivo'
+
+console.log(message)
